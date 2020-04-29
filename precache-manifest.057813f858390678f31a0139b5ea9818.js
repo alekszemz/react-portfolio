4016435cae434c980ea9c9d2dc4a5c3161936007
alexzemz <48617088+alexzemz@users.noreply.github.com>
2020-04-29 18:22:16 +0100

    Updates

---
+++ precache-manifest.057813f858390678f31a0139b5ea9818.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "203fe1cf7424e2cd19c047cc9ff3fb5a",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "31ef27b2a9bb283f14ba",
+    "url": "/react-portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "625d172b0b19aa27c0ac",
+    "url": "/react-portfolio/static/css/main.5bd29698.chunk.css"
+  },
+  {
+    "revision": "31ef27b2a9bb283f14ba",
+    "url": "/react-portfolio/static/js/2.c909573c.chunk.js"
+  },
+  {
+    "revision": "c78c651f690300193655e38a604668c9",
+    "url": "/react-portfolio/static/js/2.c909573c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "625d172b0b19aa27c0ac",
+    "url": "/react-portfolio/static/js/main.116ce6a3.chunk.js"
+  },
+  {
+    "revision": "0346f5e54be923396aed",
+    "url": "/react-portfolio/static/js/runtime-main.8a7d631b.js"
+  }
+]);
